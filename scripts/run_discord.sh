c0dd5d71d5a994e7a2b07d030bd0d2dbbfad0104
Salvatore <santorosalvatoreunina@gmail.com>
2024-12-09 17:41:15 +0100

    Commit of lun, 09 dic 2024 17:41:15 +0100

--- scripts/run_discord.sh
+++ scripts/run_discord.sh
@@ -6,5 +6,8 @@ else
 	echo "Virtual device already existing"
 fi
 obs --startvirtualcam > /dev/null 2>&1 &
-env XDG_SESSION_TYPE=x11 discord --force-device-scale-factor=2.0 > /dev/null 2>&1  &
+discord --disable-gpu-compositing --enable-features=UseOzonePlatform --ozone-platform=wayland
+
+#https://www.reddit.com/r/wayland/comments/1cywhh2/anyone_still_seeing_flickering_in/
+#env XDG_SESSION_TYPE=x11 discord --force-device-scale-factor=2.0 > /dev/null 2>&1  &
 
