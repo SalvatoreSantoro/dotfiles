b72adfcaefee25504ad08627cefd22c62c612bf1
Salvatore <santorosalvatoreunina@gmail.com>
2025-01-03 00:33:55 +0100

    Commit of ven, 03 gen 2025 00:33:54 +0100

--- dotfiles.sh
+++ dotfiles.sh
@@ -21,7 +21,7 @@ echo "           "
 
 home_directory="$HOME/.config"
 current_directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-modules=(".zshrc" "sway" "hypr" "kitty" "neofetch" "rofi" "waybar" "wallpapers" "nvim" "swaync" "wofi" "nwg-bar")
+modules=(".zshrc" "sway" "hypr" "kitty" "neofetch" "rofi" "waybar" "wallpapers" "nvim" "swaync" "wofi" "nwg-bar" "dunst")
 
 read operation
 
