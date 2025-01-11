252d98ed288815659bd086340bd95050a357f764
Salvatore <santorosalvatoreunina@gmail.com>
2025-01-11 10:29:07 +0100

    Commit of sab, 11 gen 2025 10:29:06 +0100

--- hypr/scripts/screenshot.sh
+++ hypr/scripts/screenshot.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 DIR=~/Immagini
 
-# Check if the directory exists, if not, create it
-
 export WLR_NO_HARDWARE_CURSORS=1 
-grim -c -g "$(slurp)" -o $DIR/$(date +%Y%m%d-%H%M%S).png 
+grim -c -g "$(slurp)" -o $DIR/$(date +%Y%m%d-%H%M%S).png && notify-send -t 3000 "Screenshot captured"
 
-
-notify-send -t 3000 "Screenshot captured"
